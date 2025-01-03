a783b8ed4bc3f108d4c45446a06276dc940bc23e
Madhu Kalapala <Madhu.Kalapala@ibm.com>
2025-01-03 23:23:48 +0530

    Dice roll game

---
+++ 07-Dice-Game/script.js
@@ -0,0 +1,93 @@
+'use strict';
+
+const scoreOne = document.querySelector('#score--0');
+const scoreTwo = document.querySelector('#score--1');
+
+const currentOne = document.querySelector('#current--0');
+const currentTwo = document.querySelector('#current--1');
+
+const dice = document.querySelector('.dice');
+const buttonNewGame = document.querySelector('.btn--new');
+const buttonDiceRoll = document.querySelector('.btn--roll');
+const buttonHold = document.querySelector('.btn--hold');
+
+const playerOne = document.querySelector('.player--0');
+const playerTwo = document.querySelector('.player--1');
+
+
+let cpOne = 0;
+let cpTwo = 0;
+let player = 1;
+let playerScoreOne = 0;
+let playerScoreTwo = 0;
+scoreOne.textContent = playerScoreOne;
+scoreTwo.textContent = playerScoreTwo;
+
+const diceRoll = function () {
+    let diceValue = Math.floor(Math.random() * 6) + 1;
+    console.log('button roll cliked', dice.src = `dice-${diceValue}.png`, diceValue);
+   
+    if (player === 1) {
+        if (diceValue == 1) {
+            player = 2;
+            cpOne = 0;
+            diceValue = 0;
+        }
+        cpOne = cpOne + diceValue;
+        console.log(typeof cpOne, typeof diceValue);
+    } else if (player === 2) {
+        if (diceValue == 1) {
+            player = 1;
+            cpTwo = 0;
+            diceValue = 0;
+        }
+        cpTwo = cpTwo + diceValue;
+      
+    }
+    currentOne.textContent = cpOne;
+    currentTwo.textContent = cpTwo;
+}
+
+buttonDiceRoll.addEventListener('click', diceRoll);
+
+buttonHold.addEventListener('click', function () {
+    if (player == 1) {
+        playerScoreOne = playerScoreOne + cpOne;
+        if (playerScoreOne >= 100) {
+            playerOne.classList.add('player--winner');
+        }
+        cpOne = 0;
+        player = 2;
+    }
+    else {
+        playerScoreTwo = playerScoreTwo + cpTwo;
+        if (playerScoreTwo >= 100) {
+            playerTwo.classList.add('player--winner');
+        }
+        cpTwo = 0;
+        player = 1;
+    }
+    scoreOne.textContent = playerScoreOne;
+    scoreTwo.textContent = playerScoreTwo;
+    currentOne.textContent = cpOne;
+    currentTwo.textContent = cpTwo;
+})
+
+buttonNewGame.addEventListener('click', function () {
+    cpOne = 0;
+    cpTwo = 0;
+    player = 1;
+    playerScoreOne = 0;
+    playerScoreTwo = 0;
+    scoreOne.textContent = playerScoreOne;
+    scoreTwo.textContent = playerScoreTwo;
+    currentOne.textContent = cpOne;
+    currentTwo.textContent = cpTwo;
+    if (playerOne.classList.contains('player--winner')) {
+        playerOne.classList.remove('player--winner');
+    }
+    if (playerTwo.classList.contains('player--winner')) {
+        playerTwo.classList.remove('player--winner');
+    }
+})
+
